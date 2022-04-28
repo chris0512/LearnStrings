41f6ff8469267b6408f5b250213d8100980c805d
chris0512 <kyang3200@gmail.com>
2022-04-28 21:34:45 +0800

    String length and safeguard for index by if state

--- src/LearnStrings.java
+++ src/LearnStrings.java
@@ -1,28 +1,13 @@
 public class LearnStrings {
     public static void main(String[] args) {
-        String text1 = "this is my text1";
-        String text2 = "this is my text2";
-//        System.out.println(text1 + " " + text2 + " hello");
+        String myText = "for";
+        System.out.println(myText.length());
+        char[] chars = myText.toCharArray();
+        int index = 2;
+        if(index < myText.length()){
+            System.out.println(chars[index]);
+        }
 
-//        System.out.println("my string literal".concat("second string literal"));
-
-        String finalString = new StringBuilder(text1.length() + text2.length() + 1)
-                .append(text1)
-                .append(" ")
-                .append(text2)
-                .toString();
-        System.out.println(finalString);
-
-        String otherFinalString = new StringBuffer()
-                .append(text1)
-                .append(" ")
-                .append(text2)
-                .toString();
-        System.out.println(otherFinalString);
-
-        System.out.format("%s %s\n", text1, text2);
-        String oneMoreFinalString = String.format("%s %s", text1, text2);
-        System.out.println(oneMoreFinalString);
     }
 
 }
