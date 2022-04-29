bd8a524c12864b8e6f3bef9351635b8256c4533d
chris0512 <kyang3200@gmail.com>
2022-04-29 12:15:24 +0800

    Phone Number Extraction

--- src/LearnStrings.java
+++ src/LearnStrings.java
@@ -1,17 +1,33 @@
 public class LearnStrings {
     public static void main(String[] args) {
-       String myText = "apple";
-        String firstPart = myText.substring(0, 1);
-        String secondPart = myText.substring(1);
-        String capitalFirstLetter = firstPart.toUpperCase();
-        int capacity = capitalFirstLetter.length() + secondPart.length();
-        String myNewText = new StringBuilder(capacity)
-                .append(capitalFirstLetter)
-                .append(secondPart)
-                .toString();
+        String phoneNumber = "(763) 872-6633";
+        String areaCode = parseAreaCode(phoneNumber);
+        String exchange = parseExchange(phoneNumber);
+        String lineNumber = parseLineNumber(phoneNumber);
 
-       System.out.println(myNewText);
+        System.out.println(areaCode);
+        System.out.println(exchange);
+        System.out.println(lineNumber);
+    }
 
+    public static String parseAreaCode(String phoneNumber){
+        int openParens = phoneNumber.indexOf("(");
+        int closeParens = phoneNumber.indexOf(")");
+        String areaCode = phoneNumber.substring(openParens+1, closeParens);
+        return areaCode;
+    }
+
+    public static String parseExchange(String phoneNumber) {
+        int spaceIdx = phoneNumber.indexOf(" ");
+        int hyphenIdx = phoneNumber.indexOf("-");
+        String exchange = phoneNumber.substring(spaceIdx+1, hyphenIdx);
+        return exchange;
+    }
+
+    public static String parseLineNumber(String phoneNumber){
+        int hyphenIdx = phoneNumber.indexOf("-");
+        String lineNumber = phoneNumber.substring(hyphenIdx+1);
+        return lineNumber;
     }
 
 }
